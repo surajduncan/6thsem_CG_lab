15e169a1a441f162dc36ab3bacc644952176f515
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-12 23:20:22 +0530

    Update breshnham_line_drawing.cpp

--- breshnham_line_drawing.cpp
+++ breshnham_line_drawing.cpp
@@ -79,3 +79,17 @@ void myDisplay()
 	draw_line(x1, x2, y1, y2);
 	glFlush();
 }
+int main(int argc, char **argv)
+{
+	printf( "Enter (x1, y1, x2, y2)\n");
+	scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Bresenham's Line Drawing");
+	myInit();
+	glutDisplayFunc(myDisplay);
+	glutMainLoop();
+	return 0;
+}
