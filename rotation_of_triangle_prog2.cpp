8e454949efc779de8c2b6707f9c647fd6329704a
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-13 22:45:43 +0530

    Update rotation_of_triangle_prog2.cpp

--- rotation_of_triangle_prog2.cpp
+++ rotation_of_triangle_prog2.cpp
@@ -26,3 +26,23 @@ void matrix3x3SetIdentity (Matrix3x3 matIdent3x3)
 		for (col = 0; col < 3; col++)
 			matIdent3x3 [row][col] = (row == col);
 }
+void matrix3x3PreMultiply (Matrix3x3 m1, Matrix3x3 m2)
+{
+	GLint row, col;
+	Matrix3x3 matTemp;
+	for (row = 0; row < 3; row++)
+		for (col = 0; col < 3 ; col++)
+			matTemp [row][col] = m1 [row][0] * m2 [0][col] + m1 [row][1] *	m2 [1][col] + m1 [row][2] * m2 [2][col];	
+	for (row = 0; row < 3; row++)
+		for (col = 0; col < 3; col++)
+			m2 [row][col] = matTemp [row][col];
+}
+
+void translate2D (GLfloat tx, GLfloat ty)
+{
+	Matrix3x3 matTransl;
+	matrix3x3SetIdentity (matTransl);
+	matTransl [0][2] = tx;
+	matTransl [1][2] = ty;
+	matrix3x3PreMultiply (matTransl, matComposite);
+}
