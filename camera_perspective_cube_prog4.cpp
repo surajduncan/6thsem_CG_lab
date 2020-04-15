bf46ad096acc06faaa162ea2b5af92261d6d86f0
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-15 23:28:28 +0530

    Update camera_perspective_cube_prog4.cpp

--- camera_perspective_cube_prog4.cpp
+++ camera_perspective_cube_prog4.cpp
@@ -39,3 +39,20 @@ GLubyte cubeIndices[]={ 0,3,2,1,
 						1,2,6,5,
 						4,5,6,7,
 						0,1,5,4};
+static GLfloat theta[] = {0.0,0.0,0.0};
+static GLint axis = 2;
+static GLdouble viewer[] = {0.0,0.0,5.0};
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	gluLookAt(viewer[0],viewer[1],viewer[2],0.0,0.0,0.0,0.0,1.0,0.0);
+	glRotatef(theta[0],1.0,0.0,0.0);
+	glRotatef(theta[1],0.0,1.0,0.0);
+	glRotatef(theta[2],0.0,0.0,1.0);
+	glDrawElements(GL_QUADS,24,GL_UNSIGNED_BYTE,cubeIndices);
+	glFlush();
+	glutSwapBuffers();
+}
+
