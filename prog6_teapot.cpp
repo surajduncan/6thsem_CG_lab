035551ab17e61cd25ae6197aa5583c05f33b8ef4
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-18 23:17:09 +0530

    Create prog6_teapot.cpp

---
+++ prog6_teapot.cpp
@@ -0,0 +1,9 @@
+#include<GL/glut.h>
+void wall(double thickness)
+{
+	glPushMatrix();
+	glTranslated(0.5,0.5*thickness, 0.5);
+	glScaled(1.0,thickness, 1.0);
+	glutSolidCube(1.0);
+	glPopMatrix();
+}
