3a17c8214506be564342190a16bbe605989c6c6b
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-18 23:18:15 +0530

    Update prog6_teapot.cpp

--- prog6_teapot.cpp
+++ prog6_teapot.cpp
@@ -15,3 +15,31 @@ void tableleg(double thick, double len)
 	glutSolidCube(1.0);
 	glPopMatrix();
 }
+void tableleg(double thick, double len)
+{
+	glPushMatrix();
+	glTranslated(0,len/2,0);
+	glScaled(thick, len, thick);
+	glutSolidCube(1.0);
+	glPopMatrix();
+}
+
+void table(double topwid, double topthick, double legthick, double leglen)
+{
+	glPushMatrix();
+	glTranslated(0,leglen,0);
+	glScaled(topwid, topthick, topwid);
+	glutSolidCube(1.0);
+	glPopMatrix();
+	double dist=0.95*topwid/2.0-legthick/2.0;
+	glPushMatrix();
+	glTranslated(dist, 0, dist);
+	tableleg(legthick, leglen);
+	glTranslated(0.0,0.0,-2*dist);
+	tableleg(legthick, leglen);
+	glTranslated(-2*dist, 0, 2*dist);
+	tableleg(legthick, leglen);
+	glTranslated(0,0,-2*dist);
+	tableleg(legthick,leglen);
+	glPopMatrix();
+}
