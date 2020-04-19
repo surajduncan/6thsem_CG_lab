c668e5783983b7a629b05983720d50493f817e01
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-19 23:24:37 +0530

    Update prog7_serpinski_gasket.cpp

--- prog7_serpinski_gasket.cpp
+++ prog7_serpinski_gasket.cpp
@@ -1,1 +1,17 @@
 
+
+
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glut.h>
+
+typedef float point[3];
+point v[]={ {0.0, 0.0, 1.0},
+			{0.0, 0.942809, -0.33333}, 
+			{-0.816497, -0.471405, -0.333333},
+			{0.816497, -0.471405, -0.333333}};
+static GLfloat theta[] = {0.0,0.0,0.0};
+int n;
