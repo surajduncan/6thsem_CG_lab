55a6940bf31b59f5d510e3dffb820700facb9410
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-20 22:20:50 +0530

    Update prog8_bezier_curve.cpp

--- prog8_bezier_curve.cpp
+++ prog8_bezier_curve.cpp
@@ -1,1 +1,21 @@
+Develop a menu driven program to animate a flag using Bezier Curve algorithm
 
+
+
+
+#include<GL/glut.h>
+#include<math.h>
+#include<stdio.h>
+
+void bezierCoefficients(int n,int *c)
+{
+	int k,i;
+	for(k=0;k<=n;k++)
+	{
+		c[k]=1;
+		for(i=n;i>=k+1;i--)
+			c[k]*=i;
+		for(i=n-k;i>=2;i--)
+			c[k]/=i;
+	}
+}
