3325e2867dfd1c9d9d7db735c031025b8d123c22
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-21 22:16:16 +0530

    Update prog9_scan_line.cpp

--- prog9_scan_line.cpp
+++ prog9_scan_line.cpp
@@ -62,3 +62,17 @@ void scanfill(float x1,float y1,float x2,float y2,float x3,float y3,float x4,flo
 	}
 }
 
+void display()
+{
+	x1=200.0;y1=200.0;x2=100.0;y2=300.0;x3=200.0;y3=400.0;x4=300.0;y4=300.0;
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0, 0.0, 1.0);
+	glBegin(GL_LINE_LOOP);
+		glVertex2f(x1,y1);
+		glVertex2f(x2,y2);
+		glVertex2f(x3,y3);
+		glVertex2f(x4,y4);
+	glEnd();
+	scanfill(x1,y1,x2,y2,x3,y3,x4,y4);
+	glFlush();
+}
