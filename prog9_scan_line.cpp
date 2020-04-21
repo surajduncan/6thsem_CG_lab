cf20236c0ed6cf574b866146abedebfcbfae8e70
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-21 22:09:39 +0530

    Update prog9_scan_line.cpp

--- prog9_scan_line.cpp
+++ prog9_scan_line.cpp
@@ -24,3 +24,20 @@ void edgedetect(float x1,float y1,float x2,float y2,int *le,int *re)
 	else
 		mx=x2-x1;
 		x=x1;
+	for(i=y1;i<=y2;i++)
+	{
+		if(x<(float)le[i])
+			le[i]=(int)x;
+		if(x>(float)re[i])
+			re[i]=(int)x;
+		x+=mx;
+	}
+}
+
+void draw_pixel(int x,int y)
+{
+	glColor3f(1.0,1.0,0.0);
+	glBegin(GL_POINTS);
+	glVertex2i(x,y);
+	glEnd();
+}
