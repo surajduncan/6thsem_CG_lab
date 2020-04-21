762accc779fe006f0581fa04857746b28ca990d4
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-21 22:09:08 +0530

    Update prog9_scan_line.cpp

--- prog9_scan_line.cpp
+++ prog9_scan_line.cpp
@@ -1,1 +1,26 @@
 Develop a menu driven program to fill the polygon using scan line algorithm
+
+
+
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glut.h>
+float x1,x2,x3,x4,y1,y2,y3,y4;
+
+void edgedetect(float x1,float y1,float x2,float y2,int *le,int *re)
+{
+	float mx,x,temp;
+	int i;
+	if((y2-y1)<0)
+	{
+		temp=y1;y1=y2;y2=temp;
+		temp=x1;x1=x2;x2=temp;
+	}
+	if((y2-y1)!=0)
+		mx=(x2-x1)/(y2-y1);
+	else
+		mx=x2-x1;
+		x=x1;
