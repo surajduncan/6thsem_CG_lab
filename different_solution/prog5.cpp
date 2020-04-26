3d28259dcbe05d3021ed57f2a5eecafa484796e0
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-26 23:14:10 +0530

    Update prog5.cpp

--- different_solution/prog5.cpp
+++ different_solution/prog5.cpp
@@ -127,5 +127,17 @@ void myinit()
 	glLoadIdentity();
 	gluOrtho2D(0.0,499.0,0.0,499.0);
 }
-
+void main(int argc, char** argv)
+{
+	printf("Enter the end points of the line: ");
+	scanf("%lf%lf%lf%lf", &x0,&y0,&x1,&y1);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("Cohen-Sutherland Line Clipping");
+	glutDisplayFunc(display);
+	myinit();
+	glutMainLoop();
+}
 
