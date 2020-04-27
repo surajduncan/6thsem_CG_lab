7e0bc6b85e18284ed7708c56f1e7726f68a16816
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-27 22:52:28 +0530

    Update prog6.cpp

--- different_solution/prog6.cpp
+++ different_solution/prog6.cpp
@@ -34,4 +34,50 @@ void table(double topwid, double topthick, double legthick, double leglen)
 	tableleg(legthick,leglen);
 	glPopMatrix();
 }
+void displaySolid(void)
+{
+	GLfloat mat_ambient[]={0.7f,0.7f,0.7f,1.0f};
+	GLfloat mat_diffuse[]={0.5f,0.5f,0.5f,1.0f};
+	GLfloat mat_specular[]={1.0f,1.0f,1.0f,1.0f};
+	GLfloat mat_shininess[]={50.0f};
+	glMaterialfv(GL_FRONT,GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT,GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT,GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT,GL_SHININESS, mat_shininess);
+
+	GLfloat lightintensity[]={0.7f,0.7f,0.7f,1.0f};
+	GLfloat lightposition[]={2.0f,6.0f,3.0f,0.0f};
+	glLightfv(GL_LIGHT0, GL_POSITION, lightposition);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightintensity);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	double winht=1.0;
+	glOrtho(-winht*64/48, winht*64/48, -winht, winht,  
+                                          0.1, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(2.3,1.3,2.0,0.0,0.25,0.0,0.0,1.0,0.0);
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	  glTranslated(0.6,0.38,0.5);
+	  glRotated(30,0,1,0);
+	  glutSolidTeapot(0.08);
+	glPopMatrix();
+	glPushMatrix();
+	  glTranslated(0.4,0,0.4);
+	  table(0.6,0.02,0.02,0.3);
+	glPopMatrix();
+	wall(0.02);
+	glPushMatrix();
+	  glRotated(90.0,0.0,0.0,1.0);
+	  wall(0.02);
+	glPopMatrix();
+	glPushMatrix();
+	  glRotated(-90.0,1.0,0.0,0.0);
+	  wall(0.02);
+	glPopMatrix();
+	glFlush();
+}
+
 
