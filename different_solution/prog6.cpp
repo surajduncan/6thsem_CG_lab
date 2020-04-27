c984970b019d2adc9a64e073cb02a86a75547755
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-27 22:50:40 +0530

    Update prog6.cpp

--- different_solution/prog6.cpp
+++ different_solution/prog6.cpp
@@ -1,1 +1,10 @@
+#include<GL/glut.h>
+void wall(double thickness)
+{
+	glPushMatrix();
+	glTranslated(0.5,0.5*thickness, 0.5);
+	glScaled(1.0,thickness, 1.0);
+	glutSolidCube(1.0);
+	glPopMatrix();
+}
 
