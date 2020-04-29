e983f0d710c36396a639c2fbe9af64909ca003ea
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-29 22:37:10 +0530

    Update prog8.cpp

--- different_solution/prog8.cpp
+++ different_solution/prog8.cpp
@@ -1,1 +1,17 @@
+#include<GL/glut.h>
+#include<math.h>
+#include<stdio.h>
+
+void bezierCoefficients(int n,int *c)
+{
+	int k,i;
+	for(k=0;k<=n;k++)
+	{
+		c[k]=1;
+		for(i=n;i>=k+1;i--)
+			c[k]*=i;
+		for(i=n-k;i>=2;i--)
+			c[k]/=i;
+	}
+}
 
