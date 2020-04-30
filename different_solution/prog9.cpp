934539ba901ed0f43d698ccfc03ff2c6ba53b198
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-04-30 22:41:16 +0530

    Update prog9.cpp

--- different_solution/prog9.cpp
+++ different_solution/prog9.cpp
@@ -9,3 +9,27 @@ void init()
 {
 	gluOrtho2D(0, 500, 0, 500);
 }
+void edge(int x0, int y0, int x1, int y1)
+{
+	if (y1<y0)
+	{
+		int tmp;
+		tmp = y1;
+		y1 = y0;
+		y0 = tmp;
+		tmp = x1;
+		x1 = x0;
+		x0 = tmp;
+	}
+	int x = x0;
+	m = (y1 - y0) / (x1 - x0);
+	for (int i = y0; i<y1; i++)
+	{
+		if (x<le[i])
+			le[i] = x;
+		if (x>re[i])
+			re[i] = x;
+		x += (1 / m);
+	}
+}
+
