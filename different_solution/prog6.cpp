44746637de8d3a2c2b1b0af8c9abb42c2d693af1
surajduncan <62278206+surajduncan@users.noreply.github.com>
2020-05-04 22:42:09 +0530

    Update prog6.cpp

--- different_solution/prog6.cpp
+++ different_solution/prog6.cpp
@@ -1,5 +1,6 @@
 //To draw a simple shaded scene consisting of a tea pot on a table. 
-Define suitably the position and properties of the light source along with the properties of the surfaces of the solid object used in the scene. Lighting and Shading
+//Define suitably the position and properties of the light source along with the properties of the surfaces of the solid object used in the scene. 
+//Lighting and Shading
 
 
 
